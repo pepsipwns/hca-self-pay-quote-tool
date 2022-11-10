601fe86a4ce9c80c0193ccbf980c0486bbcd5dd4
pepsipwns <anthonybrown2002@hotmail.com>
2022-11-10 18:49:39 +0000

    Storybook setup, with severe warnings, but these are only on devDependencies and everything is functioning correctly.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  purge: [
+  content: [
     // This is not present inside the default configuration
     // but it's good to build your production application
     // Read more about this here: https://tailwindcss.com/docs/installation#building-your-css
     "./pages/**/*.tsx",
+    "./stories/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
